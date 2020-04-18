7ac06f94608b115fbed6009114d4ba0b16075ad4
risist <risistt@gmail.com>
2020-04-18 19:56:42 +0200

    mostly character mechanics

--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Fireball : MonoBehaviour
 {
     public Transform target;
+    public bool bFreeForm;
+    public int testKeyCode;
+    [Header("Free form")]
     public float freeDistance;
     public float maxDistance;
     public float correctionFactor;
+
+    [Header("Follow strict")]
+    public float closeDistance = 0.5f;
+    public float farDistance = 0.5f;
+    [Range(0, 1)] public float strictLerpFactorClose = 0.1f;
+    [Range(0, 1)] public float strictLerpFactorFar = 0.1f;
+    
     Rigidbody2D body;
 
 
@@ -16,13 +26,14 @@ public class Fireball : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         transform.parent = null;
     }
-    void FixedUpdate()
+
+    void FreeFormUpdate()
     {
         Vector3 toTarget = target.position - transform.position;
         toTarget.z = 0;
         if (toTarget.sqrMagnitude < freeDistance * freeDistance)
         {
-            body.velocity *= 0.95f;
+            body.velocity *= 0.85f;
             return;
         }
         if (toTarget.sqrMagnitude > maxDistance * maxDistance)
@@ -42,6 +53,37 @@ public class Fireball : MonoBehaviour
             Vector2 force = new Vector2(toTarget.x, toTarget.y) * correctionFactor;// + Random.insideUnitCircle*1.2f*correctionFactor;
             body.AddForce(force);
         }
+    }
+
+    bool StrictUpdate()
+    {
+        float dist = Vector2.Distance(transform.position, target.position);
+        bool bIsClose = dist < closeDistance;
+        bool bIsFar = dist < farDistance;
+        if (bIsFar)
+        {
+            float lerpFactor = bIsClose ? strictLerpFactorClose : 
+                Mathf.Lerp(strictLerpFactorClose,0, dist/(farDistance));
+
+            if (bIsClose)
+                body.velocity = Vector2.zero;
+
+            /*Debug.Log(
+                (dist) / (farDistance) + " |||| " +
+                dist + " |||| " + (closeDistance - dist) + " |||| " + (farDistance- closeDistance) );*/
+            lerpFactor = Mathf.Clamp01(lerpFactor);
+            //lerpFactor = Mathf.Min(lerpFactor, strictLerpFactorFar);
+            //lerpFactor = strictLerpFactorClose;
+            transform.position = Vector3.Lerp(transform.position, target.position, lerpFactor);
+        }
+        return bIsClose;
+    }
+    void FixedUpdate()
+    {
+
+        bFreeForm = Input.GetMouseButton(testKeyCode);
+        if (bFreeForm || !StrictUpdate())
+            FreeFormUpdate();
     }
 
     private void OnDrawGizmos()
--- Assets/Scripts/Input/InputRecorderMouseKeyboard.cs
+++ Assets/Scripts/Input/InputRecorderMouseKeyboard.cs
@@ -22,23 +22,19 @@ public class InputRecorderMouseKeyboard : InputRecorder
 
         
 
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        inputHolder.rotationInput = inputHolder.directionInput = mousePosition - (Vector2)transform.position;
+
         /// position
+        /*inputHolder.positionInput = GetStrifeInput(
+            inputHolder.rotationInput.normalized, 
+            new Vector2(
+                Input.GetAxis(binding.positionAxisCodeX), 
+                Input.GetAxis(binding.positionAxisCodeY)).normalized)
+            ;*/
         inputHolder.positionInput.x = Input.GetAxis(binding.positionAxisCodeX);
         inputHolder.positionInput.y = Input.GetAxis(binding.positionAxisCodeY);
-
-        /// direction
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        var box = GetComponent<BoxCollider>();
-
-        if (box && box.Raycast(ray, out hit, float.PositiveInfinity))
-        {
-            Vector3 v = hit.point;
-            inputHolder.directionInput = new Vector2(v.x - inputHolder.transform.position.x, v.y - inputHolder.transform.position.y);
-
-        }
-        else
-            Debug.LogWarning("player direction raycast does not hit a collider");
 
         /// keys
         int n = Mathf.Min(inputHolder.keys.Length, binding.keyAxisCode.Length);
@@ -46,5 +42,43 @@ public class InputRecorderMouseKeyboard : InputRecorder
         {
             inputHolder.keys[i] = Input.GetButton(binding.keyAxisCode[i]);
         }
+    }
+
+    Vector2 GetStrifeInput(Vector2 directionInput, Vector2 positionInput)
+    {
+        return directionInput * positionInput.y +
+               new Vector2(directionInput.y, -directionInput.x) * positionInput.x;
+    }
+    Vector2 GetAllignedDirection(Vector2 directionInput, Vector2 positionInput)
+    {
+        Vector2[] directions = {
+            directionInput,
+            -directionInput,
+            new Vector2(directionInput.y, -directionInput.x),
+            new Vector2(-directionInput.y, directionInput.x)
+        };
+
+
+        Vector2 inputDirection = transform.up;
+
+        float maxDot = float.NegativeInfinity;
+        inputDirection = Vector2.zero;
+        int currentDirectionId = 0;
+        if (positionInput.sqrMagnitude < inputHolder.inputThreshold * inputHolder.inputThreshold)
+            return inputDirection;
+
+        for (int i = 0; i < directions.Length; ++i)
+        {
+            var newDot = Vector2.Dot(positionInput, directions[i]);
+            Debug.Log(newDot);
+            if (newDot > maxDot)
+            {
+                maxDot = newDot;
+                inputDirection = directions[i];
+                currentDirectionId = i;
+            }
+        }
+
+        return inputDirection;
     }
 }
