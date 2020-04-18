2f2d748d51686089a5db75191b480d076f0ee376
Jakub Rogacz <jakub.rogacz666@gmail.com>
2020-04-18 21:03:45 +0200

    Scripts for last commit

---
+++ Assets/Scripts/Mechanisms/Mechanism.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mechanism : MonoBehaviour {
+    [System.Serializable]
+    public class Querry : UnityEngine.Events.UnityEvent<string> { 
+        //implements UnityEvent that 
+    }
+    public Querry callback;
+    public string dynamicQuerry;
+
+    void trip()
+    {
+        callback.Invoke(dynamicQuerry);
+    }
+
+}
