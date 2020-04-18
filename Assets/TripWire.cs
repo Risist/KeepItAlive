1e0bd89d76076fd1488e5daca0ca192edad1ef88
Jakub Rogacz <jakub.rogacz666@gmail.com>
2020-04-18 22:05:22 +0200

    Fix for this script with trip wires

--- Assets/TripWire.cs
+++ Assets/TripWire.cs
@@ -8,6 +8,11 @@ public class TripWire : MonoBehaviour
     public GameObject portEnd;
     public LineRenderer lineRenderer;
     public bool tripped = false;
+    public LayerMask maskLayers;
+
+    public void Start() {
+        lineRenderer.positionCount = 2;
+    }
     public void Update() {
         if (!tripped)
         {
@@ -16,13 +21,19 @@ public class TripWire : MonoBehaviour
             Vector3 direction = portEnd.transform.position - portStart.transform.position;
             lineRenderer.SetPosition(0, portStart.transform.position);
             lineRenderer.SetPosition(1, portEnd.transform.position);
-            if (Physics2D.Raycast(origin, direction, direction.magnitude))
+            if (Physics2D.Raycast(origin, direction, direction.magnitude,maskLayers))
             {
                 tripped = true;
 
             }
 
 
+        }
+        else {
+            Vector3 origin = portStart.transform.position;
+            Vector3 direction = portEnd.transform.position - portStart.transform.position;
+            lineRenderer.SetPosition(0, portStart.transform.position);
+            lineRenderer.SetPosition(1, portStart.transform.position);
         }
     }
 
