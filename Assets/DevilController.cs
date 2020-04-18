fef619635630ee9b0493fc972be0b78f2b25108b
risist <risistt@gmail.com>
2020-04-18 12:58:28 +0200

    fireball, devil

---
+++ Assets/DevilController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevilController : MonoBehaviour
+{
+    GameObject player;
+    new Animator animator;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+
+    }
+
+    void LookAtPlayer(float lerpScale)
+    {
+        if (!player)
+        {
+            Debug.LogWarning("No player set up");
+            return;
+        }
+        Vector2 toPlayer = transform.position - player.transform.position;
+        float desiredAngle = Vector2.SignedAngle(Vector2.up, toPlayer);
+
+        float currentAngle = transform.rotation.eulerAngles.z;
+        float finalAngle = Mathf.LerpAngle(currentAngle, desiredAngle, lerpScale);
+
+        transform.rotation = Quaternion.Euler(0, 0, finalAngle);
+    }
+
+    void RandomBlink(float chance)
+    {
+        if(Random.value < chance)
+            animator.SetTrigger("CanBlink");
+    }
+
+    void MoveTo(Vector2 destination, float movementSpeed, float rotationSpeed)
+    {
+
+    }
+
+    void Update()
+    {
+        LookAtPlayer(0.1f);
+        RandomBlink(0.00075f);
+    }
+}
---
+++ Assets/Fireball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public Transform target;
+    public float freeDistance;
+    public float maxDistance;
+    public float correctionFactor;
+    Rigidbody2D body;
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        transform.parent = null;
+    }
+    void FixedUpdate()
+    {
+        Vector3 toTarget = target.position - transform.position;
+        toTarget.z = 0;
+        if (toTarget.sqrMagnitude < freeDistance * freeDistance)
+        {
+            body.velocity *= 0.95f;
+            return;
+        }
+        if (toTarget.sqrMagnitude > maxDistance * maxDistance)
+        {
+            body.velocity *= 0.9f;//Vector2.zero;
+            //body.position = target.position - toTarget.normalized * maxDistance;
+
+            toTarget.Normalize();
+            Vector2 force = new Vector2(toTarget.x, toTarget.y) * correctionFactor * 5.25f;
+            body.AddForce(force);
+            return;
+        }
+
+
+        {
+            toTarget.Normalize();
+            Vector2 force = new Vector2(toTarget.x, toTarget.y) * correctionFactor;// + Random.insideUnitCircle*1.2f*correctionFactor;
+            body.AddForce(force);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(target.position, maxDistance);
+    }
+
+
+}
