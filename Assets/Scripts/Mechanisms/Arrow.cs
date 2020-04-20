899d302ceb6ee26c7f89d8fad4665e2068aa4d11
Jakub Rogacz <jakub.rogacz666@gmail.com>
2020-04-20 04:51:51 +0200

    Graphical fix + pleasure plates

--- Assets/Scripts/Mechanisms/Arrow.cs
+++ Assets/Scripts/Mechanisms/Arrow.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Arrow : MonoBehaviour
+public class Arrow : Spawnable
 {
-    public float damage;
-    void OnTriggerEnter2D(Collider2D col) {
-        DamageData data = new DamageData();
-        data.damage = damage;
-        data.direction = (transform.position - col.transform.position).normalized;
-        data.position = (transform.position);
-        data.staggerIncrease = 0.3f;
-        var hp = col.GetComponent<IDamagable>();
-        
-        hp.DealDamage(data);
+    public bool isPiercing;
+
+    public override void SetupLiftime()
+    {
+        DefaultLifetime();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(!isPiercing) Destroy(gameObject);
+    }
+    void OnTriggerEnter2D(Collision2D collision){ 
+    
     }
 }
--- Assets/Scripts/Mechanisms/Spawnable.cs
+++ Assets/Scripts/Mechanisms/Spawnable.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
-public class Spawnable : MonoBehaviour { 
+public abstract class Spawnable : MonoBehaviour {
+
+    public float lifetime = 1.0f;
 
     public Spawnable spawn(Vector2 position, Vector2 velocity, Vector2 direction) {
         Spawnable spwn =  Instantiate(this, position, Quaternion.AngleAxis(Vector2.Angle(Vector2.up,direction),-Vector3.forward));
         Rigidbody2D rigid = spwn.GetComponent<Rigidbody2D>();
         rigid.velocity = velocity;
-
+        spwn.SetupLiftime();
         return spwn;
     }
+
+    public abstract void SetupLiftime();
+    protected void DefaultLifetime() {
+            Destroy(gameObject, lifetime);
+    }
+
 }
 
 
